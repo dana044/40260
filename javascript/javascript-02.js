a62c785fb2001822ed10510da0dd567388b00961
dana044 <dana044car@gmail.com>
2026-05-07 13:41:38 -0600

    calculadora inicial

---
+++ javascript/javascript-02.js
@@ -0,0 +1,43 @@
+let resultado = document.getElementById("resultado")
+let botones = document.getElementsByTagName("button")
+for (const key in botones) {
+    if (Object.prototype.hasOwnProperty.call(botones, key)) {
+        const boton = botones[key];
+        if (boton.className != "operadores") 
+            boton.addEventListener("click", pintar)
+        else
+            boton.addEventListener("click", pintar2)
+        // console.log(boton)
+    }
+}
+
+function pintar(e){
+    console.log(e.target.innerText)
+    resultado.value += e.target.innerText
+}
+
+let operadores = document.getElementsByClassName("operadores")
+for (const key in operadores) {
+    if (Object.prototype.hasOwnProperty.call(operadores, key)) {
+        const operador = operadores[key];
+        operador.addEventListener("click", pintar2)
+        // console.log(operador)
+    }   
+}   
+
+function pintar2(e){
+    console.log(e.target.innerText)
+    if (e.target.innerText == "+" )
+        suma()
+}
+
+let prm1
+function suma(){
+    prm1 = resultado.value
+    resultado.value = ""
+}
+
+function igual(){
+    prm2 = resultado.value
+    resultado.value = parseInt(prm1) + parseInt(prm2)
+}
